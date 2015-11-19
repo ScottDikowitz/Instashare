bd9bc3dd76d288a5c9744df776ad9a3c1056a7f3
Scott Dikowitz <dikowitz@gmail.com>
2015-11-19 09:05:21 -0500

    Change seeds.

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,8 +7,8 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 
-Post.create({image: "1.jpg", caption: "hello world1", user_id: 1})
-Post.create({image: "2.jpg", caption: "hello world2", user_id: 1})
-Post.create({image: "6.jpg", caption: "hello world3", user_id: 1})
-Post.create({image: "22.jpg", caption: "hello world4", user_id: 1})
-Post.create({image: "44.jpg", caption: "hello world5", user_id: 2})
+Post.create({caption: "hello world1", user_id: 1})
+Post.create({caption: "hello world2", user_id: 1})
+Post.create({caption: "hello world3", user_id: 1})
+Post.create({caption: "hello world4", user_id: 1})
+Post.create({caption: "hello world5", user_id: 2})
