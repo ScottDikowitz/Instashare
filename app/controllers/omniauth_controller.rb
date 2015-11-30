45b8ef7a327088ea05f210b8a71129cfb7af6b49
Scott Dikowitz <dikowitz@gmail.com>
2015-11-30 13:42:47 -0500

    Remove byebug from facebook controller.

--- app/controllers/omniauth_controller.rb
+++ app/controllers/omniauth_controller.rb
@@ -3,7 +3,6 @@ class OmniauthController < ApplicationController
 
   def facebook
     @user = User.find_or_create_by_auth_hash(request.env['omniauth.auth'])
-    byebug
     sign_in(@user)
     redirect_to root_url + "#/feed"
   end
