becc8936384778f034dd815a1c609c01a7be59ce
Scott Dikowitz <dikowitz@gmail.com>
2016-01-31 21:00:07 -0500

    Display accurate number of likes/comments on hover.

--- app/assets/javascripts/components/post_grid_square.js.jsx
+++ app/assets/javascripts/components/post_grid_square.js.jsx
@@ -7,6 +7,6 @@ var PostGridSquare = React.createClass ({
 
 
   render: function(){
-    return <a href={"#/posts/" + this.props.post.id} className="post-grid-square"><span className="square-info">1❤  1✉</span><div className="square-hover"><img className="grid-img" src={this.props.post.image}/></div></a>;
+    return <a href={"#/posts/" + this.props.post.id} className="post-grid-square"><span className="square-info">{this.props.post.numLikes}❤  {this.props.post.numComments}✉</span><div className="square-hover"><img className="grid-img" src={this.props.post.image}/></div></a>;
   }
 });
--- app/views/api/users/_post.json.jbuilder
+++ app/views/api/users/_post.json.jbuilder
@@ -1,2 +1,4 @@
 json.extract! post, :id, :caption
+json.numLikes post.likes.length
+json.numComments post.comments.length
 json.image asset_path(post.image.url(:medium))
