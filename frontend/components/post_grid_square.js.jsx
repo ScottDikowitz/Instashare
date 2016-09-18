d0531555442010b405b22e9571bcfd744a70847d
Scott <dikowitz@gmail.com>
2016-09-18 18:22:45 -0400

    center cover grid images.

--- frontend/components/post_grid_square.js.jsx
+++ frontend/components/post_grid_square.js.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 var PostGridSquare = React.createClass ({
   render: function(){
-    return <a href={"#/posts/" + this.props.post.id} className="post-grid-square"><span className="square-info">{this.props.post.numLikes}❤  {this.props.post.numComments}✉</span><div className="square-hover"><img className="grid-img" src={this.props.post.image}/></div></a>;
+    return <a href={"#/posts/" + this.props.post.id} className="post-grid-square"><span className="square-info">{this.props.post.numLikes}❤  {this.props.post.numComments}✉</span><div className="square-hover"><div className="grid-img" style={{backgroundImage: `url(${this.props.post.image})`}}/></div></a>;
   }
 });
 
