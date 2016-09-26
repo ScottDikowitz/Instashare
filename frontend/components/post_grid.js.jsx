a69b9640d1a1093c00aae555468ac5965224af48
Scott <dikowitz@gmail.com>
2016-09-26 01:19:19 -0400

    add lodash and responsive grid.

--- frontend/components/post_grid.js.jsx
+++ frontend/components/post_grid.js.jsx
@@ -2,15 +2,39 @@ import React from 'react';
 
 import PostGridSquare from './post_grid_square.js';
 
-var PostGrid = React.createClass ({
-  render: function(){
+class PostGrid extends React.Component {
+  constructor (props) {
+    super(props);
 
-    return <div className="post-grid group">
-            {this.props.posts.map(function(post){
-            return <PostGridSquare key={post.id} post={post} />;
-          })}
-          </div>;
+    this._constructGrid = this._constructGrid.bind(this);
   }
-});
+
+  render () {
+      const collection = this._constructGrid();
+      return <div className="post-grid">
+                {collection}
+              </div>;
+  }
+
+  _constructGrid () {
+      var collection = [];
+      const {length} = this.props.posts;
+      for (var i = 0; i < length; i+= 3){
+          let row = [];
+          let post1 = this.props.posts[i];
+          row.push(<PostGridSquare key={`post-${post1.id}`} post={post1} style={{marginLeft: 10}}/>);
+          if (i + 1 < length) {
+              let post2 = this.props.posts[i + 1];
+              row.push(<PostGridSquare key={`post-${post2.id}`} post={post2} style={{margin: '0 10px 0 10px'}}/>);
+          } else { row.push(<div key={'spacer-1'} style={{display: 'flex', flex: 1}}></div>)}
+          if (i + 2 < length) {
+              let post3 = this.props.posts[i + 2];
+              row.push(<PostGridSquare key={`post-${post3.id}`} post={post3} style={{marginRight: 10}}/>);
+          } else { row.push(<div key={'spacer-2'} style={{display: 'flex', flex: 1}}></div>)}
+          collection.push(<div style={{display: 'flex', flex: 1, marginBottom: 10}} key={`grid-row-${i}`}>{row}</div>);
+      }
+      return collection;
+  }
+};
 
 export default PostGrid;
--- frontend/components/post_grid_square.js.jsx
+++ frontend/components/post_grid_square.js.jsx
@@ -1,8 +1,19 @@
 import React from 'react';
+import merge from 'lodash/merge';
 
 var PostGridSquare = React.createClass ({
   render: function(){
-    return <a href={"#/posts/" + this.props.post.id} className="post-grid-square"><span className="square-info">{this.props.post.numLikes}❤  {this.props.post.numComments}✉</span><div className="square-hover"><div className="grid-img" style={{backgroundImage: `url(${this.props.post.image})`}}/></div></a>;
+    //   <div className="grid-img" style={{backgroundImage: `url(${this.props.post.image})`}}/>
+    return <a href={"#/posts/" + this.props.post.id}
+              className="post-grid-square"
+              style={merge({},this.props.style)}>
+              <span className="square-info">
+                  {this.props.post.numLikes}❤  {this.props.post.numComments}✉
+              </span>
+              <div className="square-hover">
+                  <img className="grid-img" src={this.props.post.image} style={{objectFit: 'cover'}}/>
+              </div>
+           </a>;
   }
 });
 
