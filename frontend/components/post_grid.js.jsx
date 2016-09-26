f8ff23d441de776602be2bd19239abbb05ca2d4f
Scott <dikowitz@gmail.com>
2016-09-26 01:21:41 -0400

    fix last element margins.

--- frontend/components/post_grid.js.jsx
+++ frontend/components/post_grid.js.jsx
@@ -26,11 +26,11 @@ class PostGrid extends React.Component {
           if (i + 1 < length) {
               let post2 = this.props.posts[i + 1];
               row.push(<PostGridSquare key={`post-${post2.id}`} post={post2} style={{margin: '0 10px 0 10px'}}/>);
-          } else { row.push(<div key={'spacer-1'} style={{display: 'flex', flex: 1}}></div>)}
+          } else { row.push(<div key={'spacer-1'} style={{display: 'flex', flex: 1, margin: '0 10px 0 10px'}}></div>)}
           if (i + 2 < length) {
               let post3 = this.props.posts[i + 2];
               row.push(<PostGridSquare key={`post-${post3.id}`} post={post3} style={{marginRight: 10}}/>);
-          } else { row.push(<div key={'spacer-2'} style={{display: 'flex', flex: 1}}></div>)}
+          } else { row.push(<div key={'spacer-2'} style={{display: 'flex', flex: 1, marginRight: 10}}></div>)}
           collection.push(<div style={{display: 'flex', flex: 1, marginBottom: 10}} key={`grid-row-${i}`}>{row}</div>);
       }
       return collection;
